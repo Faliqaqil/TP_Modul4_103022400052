71594212be1f985cfa1c27fb7b99ededc7d77ebf
Faliqaqil <faliq.aqil.ra@gmail.com>
2026-03-15 10:50:59 +0700

    second commit

--- TP_MODUL4_103022400052/Program.cs
+++ TP_MODUL4_103022400052/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL4_103022400052
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Teknik Table-Driven ===");
+            string kelurahan = "Batununggal";
+            string kodePos = KodePos.getKodePos(kelurahan);
+            Console.WriteLine($"Kode Pos {kelurahan} adalah {kodePos}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ source/KodePos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TP_MODUL4_103022400052
+
+public class KodePos
+{
+    public static string getKodePos(string kelurahan)
+    {
+        Dictionary<string, string> tabelKodePos = new Dictionary<string, string>()
+        {
+            {"Batununggal", "40266"},
+            {"Kujangsari", "40287"},
+            {"Mengger", "40267"},
+            {"Wates", "40256"},
+            {"Cijaura", "40287"},
+            {"Jatisari", "40286"},
+            {"Margasari", "40286"},
+            {"Sekejati", "40286"},
+            {"Kebonwaru", "40272"},
+            {"Maleer", "40274"}
+        };
+
+        if (tabelKodePos.ContainsKey(kelurahan))
+        {
+            return tabelKodePos[kelurahan];
+        }
+        else
+        {
+            return "Kode Pos tidak ditemukan";
+        }
+    }
+}
